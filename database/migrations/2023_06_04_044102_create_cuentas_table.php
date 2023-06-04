0a7b4b14071269a8d0a8e3af5ee1719dbedba3dd
GuadalupFranco <franco.ramirez.guadalup@gmail.com>
2023-06-03 23:04:51 -0600

    agregar migraciones

---
+++ database/migrations/2023_06_04_044102_create_cuentas_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('cuentas', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('tipo_cuenta_id');
+            $table->integer('clave');
+            $table->string('nombre');
+            $table->double('saldo_inicial', 8, 2);
+            $table->double('saldo_actual', 8, 2);
+            $table->timestamps();
+            $table->foreign('tipo_cuenta_id')->references('id')->on('tipo_cuentas');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('cuentas');
+    }
+};
---
+++ database/migrations/2023_06_04_046644_create_detalle_transacciones_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('detalle_transacciones', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('cuenta_id');
+            $table->unsignedBigInteger('transaccion_id');
+            $table->double('monto', 8, 2);
+            $table->timestamps();
+            $table->foreign('cuenta_id')->references('id')->on('cuentas');
+            $table->foreign('transaccion_id')->references('id')->on('transacciones');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('detalle_transacciones');
+    }
+};
