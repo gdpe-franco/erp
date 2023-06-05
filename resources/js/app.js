b06e6d99b9d7ce6460e6be351371d9ebb8418658
GuadalupFranco <franco.ramirez.guadalup@gmail.com>
2023-06-05 07:16:18 -0600

    agregar dependencias y configuración del app.js

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,12 +1,29 @@
 import './bootstrap';
 import '../css/app.css';
 
+//theme
+import "primevue/resources/themes/lara-light-indigo/theme.css";     
+    
+//core
+import "primevue/resources/primevue.min.css";
+
 import { createApp, h } from 'vue';
 import { createInertiaApp } from '@inertiajs/vue3';
+import { createPinia } from 'pinia'
+import { createRouter, createWebHashHistory } from 'vue-router';
 import { resolvePageComponent } from 'laravel-vite-plugin/inertia-helpers';
 import { ZiggyVue } from '../../vendor/tightenco/ziggy/dist/vue.m';
 
+import PrimeVue from 'primevue/config';
+
 const appName = window.document.getElementsByTagName('title')[0]?.innerText || 'Laravel';
+
+const pinia = createPinia()
+
+const router = createRouter({
+    history: createWebHashHistory(),
+    routes: []
+})
 
 createInertiaApp({
     title: (title) => `${title} - ${appName}`,
@@ -15,6 +32,9 @@ createInertiaApp({
         return createApp({ render: () => h(App, props) })
             .use(plugin)
             .use(ZiggyVue, Ziggy)
+            .use(PrimeVue)
+            .use(pinia)
+            .use(router)
             .mount(el);
     },
     progress: {
